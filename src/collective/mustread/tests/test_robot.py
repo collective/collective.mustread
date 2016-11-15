f9e83b882ab202fe22887ca1658ee05ce528557f
Guido A.J. Stevens <guido.stevens@cosent.nl>
2016-11-15 11:51:16 +0100

    Fix code-analysis breakage already shipped with the template :-(

--- src/collective/mustread/tests/test_robot.py
+++ src/collective/mustread/tests/test_robot.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
+from collective.mustread.testing import COLLECTIVE_MUSTREAD_ACCEPTANCE_TESTING  # noqa
 from plone.app.testing import ROBOT_TEST_LEVEL
 from plone.testing import layered
-from collective.mustread.testing import COLLECTIVE_MUSTREAD_ACCEPTANCE_TESTING  # noqa
 
 import os
 import robotsuite
